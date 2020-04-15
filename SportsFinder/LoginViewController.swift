96434e7bc076da9da41c2a543fb11511c5f6d04e
Liiban <Liibanmukhtar@hotmail.co.uk>
2020-04-15 15:00:32 +0100

    commit for login page / new view controller

---
+++ SportsFinder/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  SportsFinder
+//
+//  Created by Liiban Mukhtar on 15/04/2020.
+//  Copyright © 2020 Liiban Mukhtar. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+    @IBAction func backButtonPressed(_ sender: Any) {
+        
+        self.dismiss(animated: true, completion: nil)
+
+    }
+    
+
+}
--- SportsFinder/ViewController.swift
+++ SportsFinder/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBOutlet weak var signInButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        signInButton.layer.cornerRadius = 40;
+        signInButton.clipsToBounds = true;
+
+        signUpButton.layer.cornerRadius = 40;
+        signUpButton.clipsToBounds = true;
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func signInButtonPressed(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "goToLogin", sender: self)
+    }
+    
 }
 
