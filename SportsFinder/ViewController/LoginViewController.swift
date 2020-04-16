2842263d6cdcca0b39ce79ed2d63cecca882ee0c
Liiban <Liibanmukhtar@hotmail.co.uk>
2020-04-16 11:34:20 +0100

    Added a incorrect login label, this is with a timer that will clear the text and the label after 3 seconds

--- SportsFinder/ViewController/LoginViewController.swift
+++ SportsFinder/ViewController/LoginViewController.swift
@@ -9,61 +9,86 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-	
-	// MARK: - Views
-	
-	@IBOutlet weak var usernameTextField: UITextField!
-	@IBOutlet weak var passwordTextField: UITextField!
-	
-	// MARK: - Properties
-	
-	let viewModel = ViewModel() // Logic
-	
-	// MARK: -  Lifecycle
-	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		
-	}
-	
-	// MARK: - Actions
-	
-	@IBAction func backButtonPressed(_ sender: Any) {
-		self.dismiss(animated: true, completion: nil)
-	}
-	
-	@IBAction func loginButtonAction(_ sender: UIButton) {
-		guard let stringUsername = usernameTextField.text, let stringPassword = passwordTextField.text else {
-			return
-		}
-		
-		// Check email & pwd
-		let isValidated = viewModel.validateUser(email: stringUsername, password: stringPassword)
-		
-		if (isValidated) {
-			// If ok ~> dismiss page and show Home Page (HomeViewController)
-		} else {
-			// if not ~> Show alert (Show a label with property .isHidden)
-		}
-		
-	}
-	
+    
+    // MARK: - Views
+    
+    @IBOutlet weak var incorrectLoginMessage: UILabel!{
+        didSet{
+            incorrectLoginMessage.isHidden = true // hide
+            
+        }
+    }
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    // MARK: - Properties
+    
+    let viewModel = ViewModel() // Logic
+    
+    // MARK: -  Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func backButtonPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func loginButtonAction(_ sender: UIButton) {
+        guard let stringUsername = usernameTextField.text, let stringPassword = passwordTextField.text else {
+            return
+        }
+        
+        // Check email & pwd
+        let isValidated = viewModel.validateUser(email: stringUsername, password: stringPassword)
+        
+        if (isValidated) {
+            // If ok ~> dismiss page and show Home Page (HomeViewController)
+        } else {
+            // if not ~> Show alert (Show a label with property .isHidden)
+            incorrectLoginMessage.isHidden = false // hide
+            
+            Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+            
+        }
+        
+        
+    }
+    
+    
+    @objc func updateUI(){
+        incorrectLoginMessage.isHidden = true // hide
+        usernameTextField.text = nil
+        passwordTextField.text = nil
+        
+    }
+    
+    
 }
 
 class ViewModel {
-	
-	// MARK: - Lifecycle
-	
-	init() {}
-	
-	// MARK: - ublic Methods
-	
-	func validateUser(email: String, password: String) -> Bool {
-		if ((email == "email@gmail.com") && (password == "password")) {
-			return true
-		}
-		
-		return false
-	}
-	
+    
+    // MARK: - Lifecycle
+    
+    init() {}
+    let tempUsername = "abc"
+    let tempPassword = "123"
+    
+    // MARK: - ublic Methods
+    
+    func validateUser(email: String, password: String) -> Bool {
+        if ((email == tempUsername) && (password == tempPassword)) {
+            return true
+        }
+        
+        return false
+    }
+    
 }
